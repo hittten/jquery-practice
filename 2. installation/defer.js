b995dd26d7190be1958e8576c75b972deddb46d6
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-02-25 11:30:11 +0100

    jquery practice

---
+++ 2. installation/defer.js
@@ -0,0 +1,7 @@
+console.log('init script')
+$('.two').text('Hello, Jquery world')
+$( document ).ready(function() {
+    console.log('document ready')
+    $('.one').text('Hello, Jquery world')
+});
+console.log('end script')
